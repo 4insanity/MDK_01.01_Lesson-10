b6fefd0bb8763fe4bc3b25734c0ffba1b67b58a9
4insanity <Aporatxop@gmail.com>
2025-01-27 15:10:50 +0300

    Add files via upload
    Задание 2

---
+++ ZADANIE2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DZ2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EnglishTranslator translator = new EnglishTranslator();
+            translator.GetLanguage();
+        }
+    }
+
+    public abstract class Translator
+    {
+        public abstract void GetLanguage();
+        
+    }
+
+    public class EnglishTranslator : Translator
+    {
+        public override void GetLanguage()
+        {
+            Console.WriteLine("Я переводчик с английского языка");
+        }
+    }
+}
