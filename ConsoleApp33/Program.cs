a6f375dacf91f5200cbcad354b739e536a82ef24
4insanity <Aporatxop@gmail.com>
2025-01-27 15:09:09 +0300

    Add files via upload
    Задание 1

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+public interface ISelectable
+{
+    public void OnSelect();
+}
+
+public interface IUpdatable
+{
+    public void Refresh();
+   
+}
+
+
+public class Screen : ISelectable, IUpdatable
+{
+
+    public void OnSelect()
+    {
+        Console.WriteLine("Выбрано");
+    }
+    public void Refresh()
+    {
+        Console.WriteLine("Обновлено");
+    }
+    
+}
+
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Screen screen = new Screen();
+        screen.OnSelect();
+        screen.Refresh();
+    }
+}
